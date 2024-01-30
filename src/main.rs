dc0663601cf523c65e5c365d332bb1785bdbf23d
Venkata Mukund Kashyap Yedunuthala <mukundkash1997@gmx.net>
2024-01-30 15:34:55 +0530

    feat: add element routine

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ mod material_routine;
 mod mesh_generator;
 mod solver;
 fn main() {
-    let _radial_nodes: Vec<f64> = mesh_generator::generate();
-    println!("The nodes are: {:?}", _radial_nodes);
-    let _exact_solution: Vec<f64> = analytical_solution::compute(_radial_nodes.clone());
+    let radial_nodes: Vec<f64> = mesh_generator::generate();
+    println!("The nodes are: {:?}", radial_nodes);
+    let _exact_solution: Vec<f64> = analytical_solution::compute(radial_nodes.clone());
     println!("The exact displacements are: {:?}", _exact_solution);
     let mat_size: usize;
     match <u32 as TryInto<usize>>::try_into(input_parameters::N_ELEMENTS) {
         Ok(u) => {
             mat_size = u;
-            solver::solver(mat_size);
+            solver::solver(mat_size, radial_nodes);
         }
         Err(_) => {
             println!("Conversion to size failed.");
--- src/solver.rs
+++ src/solver.rs
@@ -1,5 +1,3 @@
-use std::os::unix::process;
-
 pub use crate::input_parameters;
 use ndarray::prelude::*;
 use ndarray_linalg::*;
@@ -16,43 +14,101 @@ fn generate_time_span() -> Vec<f64> {
     result
 }
 
-pub fn solver(mat_size: usize) {
+pub fn solver(mat_size: usize, radial_nodes: Vec<f64>) {
+    // Initial variables
     let mut _load_scale: f64 = 0.0;
     let mut load: f64 = 0.0;
+    let mut sigma_ov: Array1<f64> = Array1::zeros(2);
+    let mut sigma: Array1<f64> = Array1::zeros(2);
+    let mut pl_sig_ov: Array1<f64> = Array1::zeros(2); //Placeholder
+    let mut g_displacements: Array1<f64> = Array1::zeros(mat_size + 1);
+    let mut pl_disp: Array1<f64> = Array1::zeros(mat_size + 1);
+    let mut displacement_evolution: Vec<f64> = Vec::new();
+
     let time_span = generate_time_span();
+    // Solver
     for (_, &t_step) in time_span.iter().enumerate() {
         // TIME LOOP
         if t_step <= input_parameters::T_LOAD_END {
-            _load_scale = (1.0/input_parameters::T_LOAD_END) * t_step;
-            load = <u32 as Into<f64>>::into(input_parameters::P_MAX * 
-                input_parameters::INNER_RADIUS) * _load_scale;
+            _load_scale = (1.0 / input_parameters::T_LOAD_END) * t_step;
+            load =
+                <u32 as Into<f64>>::into(input_parameters::P_MAX * input_parameters::INNER_RADIUS)
+                    * _load_scale;
         } else {
-            load = <u32 as Into<f64>>::into(input_parameters::P_MAX * 
-                input_parameters::INNER_RADIUS);
+            load =
+                <u32 as Into<f64>>::into(input_parameters::P_MAX * input_parameters::INNER_RADIUS);
         }
-        let mut newton_iterations:u32 = 0;
+        let mut newton_iterations: u32 = 0;
         while newton_iterations <= input_parameters::MAX_ITERATIONS {
             // NEWTON LOOP
-            let mut e_stiffness: Array2<f64> = Array2::zeros((2,2));
-            let mut g_stiffness: Array2<f64> = Array2::zeros((mat_size+1, mat_size+1));
+            let mut e_stiffness: Array2<f64> = Array2::zeros((2, 2));
+            let mut g_stiffness: Array2<f64> = Array2::zeros((mat_size + 1, mat_size + 1));
             let mut e_forces: Array1<f64> = Array1::zeros(2);
-            let mut g_int_forces: Array1<f64> = Array1::zeros(mat_size+1);
-            let mut g_ext_forces: Array1<f64> = Array1::zeros(mat_size+1);
-            let mut delta_u:Array1<f64> = Array1::zeros(mat_size+1);
+            let mut g_int_forces: Array1<f64> = Array1::zeros(mat_size + 1);
+            let mut g_ext_forces: Array1<f64> = Array1::zeros(mat_size + 1);
+            let mut delta_u: Array1<f64> = Array1::zeros(mat_size + 1);
             g_ext_forces[0] = load;
             let mut sigma_evolution: Vec<f64> = Vec::new();
             // Element routine
+            for element in 0..(mat_size) {
+                let mut r1 = radial_nodes[element];
+                let mut r2 = radial_nodes[element + 1];
+                let mut r_nodes: Array1<f64> = Array1::zeros(2);
+                r_nodes[0] = r1;
+                r_nodes[1] = r2;
+                let mut e_displacement: Array1<f64> = Array1::zeros(2);
+                e_displacement[0] = g_displacements[element];
+                e_displacement[1] = g_displacements[element + 1];
+                let mut delta_u_element: Array1<f64> = Array1::zeros(2);
+                delta_u_element[0] = pl_disp[element];
+                delta_u_element[1] = pl_disp[element + 1];
+                let mut prev_sigma_ov = sigma_ov.clone();
+                let mut jacobian = (r2 - r1) / 2.0;
+                let xi: f64 = input_parameters::N_INTEGRATION_POINTS.into();
+                let mut shape_fn: Array1<f64> = Array1::zeros(2);
+                shape_fn[0] = 0.5 * (1.0 - xi);
+                shape_fn[1] = 0.5 * (1.0 + xi);
+                let strain_disp_element1 = -1.0 / (2.0 * jacobian);
+                let strain_disp_element2 = 1.0 / (2.0 * jacobian);
+                let strain_disp_element3 = 1.0 / (r1 + r2);
+                let strain_disp_element4 = 1.0 / (r1 + r2);
+                let b_mat: Array2<f64> = Array::from_shape_vec(
+                    (2, 2),
+                    vec![
+                        strain_disp_element1,
+                        strain_disp_element2,
+                        strain_disp_element3,
+                        strain_disp_element4,
+                    ],
+                )
+                .unwrap();
+                let strain = b_mat.dot(&e_displacement);
+                let delta_strain = b_mat.dot(&delta_u_element);
+                // Material Routine
+                let mut tangent_stiffness: Array2<f64> = Array2::zeros((2, 2));
+                // Element stiffness
+                e_forces = 2.0 * b_mat.t().dot(&sigma) * shape_fn.t().dot(&r_nodes) * jacobian;
+                e_stiffness = 2.0
+                    * jacobian
+                    * shape_fn.t().dot(&r_nodes)
+                    * b_mat.t().dot(&(tangent_stiffness.dot(&b_mat)));
+            }
+            // Assemble
             // Solver
             let g_forces: Array1<f64> = g_ext_forces - g_int_forces;
             match g_stiffness.solve(&g_forces) {
                 Ok(x) => {
                     delta_u = x;
+                    for (u, &del_u) in g_displacements.iter_mut().zip(delta_u.iter()) {
+                        *u += del_u;
+                    }
+                    pl_disp = delta_u.clone();
+                    // Newton convergence criteria
                 }
                 Err(e) => {
                     panic!("Panic. Error: {}", e);
                 }
             }
-            // let _x = g_stiffness.solve(&g_forces);
         }
     }
 }
